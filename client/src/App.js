e8e431e7ffa8afb018f55b19809c307f72c4622f
Christian Lagasca <cjrl998@icloud.com>
2019-11-21 13:54:22 -0500

    test by added name

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 import { Helmet } from 'react-helmet';
@@ -7,21 +6,11 @@ import { Helmet } from 'react-helmet';
 function App() {
   return (
     <div className="App">
-      {/* NOTE: Keep the helmet code below and change the app title.
-          Add any meta tags you want, or any tag you want to go in the header. 
-          You can modify everything else! */}
       <Helmet>
-        <title>Your App Title Here</title>
+        <title>We are Winning this </title>
       </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Brainstation 3D Starter</h2>
-      </header>
+          <p>Hello</p>
     </div>
   );
 }
-
 export default App;
